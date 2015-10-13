f86aae89b2e051b54fb77b699612299b3635e817
Maxim <kul-astra@ya.ru>
2015-10-13 14:42:41 +0300

    successfuly added new branch battleships

--- js_projects/Battleship/battleship.js
+++ js_projects/Battleship/battleship.js
@@ -138,15 +138,3 @@ function handleKeyPress(e)
         return false;
     }
 }
-
-//---------------------
-/*controller.processGuess("A0");
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");*/
